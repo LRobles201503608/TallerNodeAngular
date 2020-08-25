27a9ee52b303cc48887b461f61dd18df680cad6a
201503608 <luisrobles2408@gmail.com>
2020-08-25 14:27:31 -0600

    clase 2 del taller de node

---
+++ server/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Funciona');
+console.log('Funciona');
+console.log('Funciona');
+console.log('Funciona');
---
+++ server/src/index.ts
@@ -0,0 +1,4 @@
+console.log('Funciona');
+console.log('Funciona');
+console.log('Funciona');
+console.log('Funciona');
